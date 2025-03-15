f3c68b655ef284c4b0983d3b015f8db6aa48aa90
Saksham Sharma <sakshamsharma1029@gmail.com>
2025-03-15 09:53:41 +0000

    updated main

--- ProgrammingLanguages/GOLang/main.go
+++ ProgrammingLanguages/GOLang/main.go
@@ -24,8 +24,28 @@ func main(){
 	fmt.Println(sum(1,2));
 	fmt.Println(sum2(1,20));
 
+	// Varialbes in Go are passed as values not as references.
+	// If we pass a variable to a function, it's value is copied to the function.
+	// If we want to pass the reference, we need to pass the address of the variable.
+	// We can pass the address of the variable by using the & operator.
+	// We can get the value of the address by using the * operator.
+
+	var1 := 10
+	increment(var1)
+	fmt.Println(var1) // This will print 10, because the value of var1 is not changed. 
+
+	fmt.Println(Getnames())
+	
+	// ignoring the return value. If function return 2 things but we only want 1 thing. 
+	// x, _ := getPoints() // the compiler completely removes from our code. 
+
 	
 }
+
+// example :- 
+// func getPoints(x int, y int) int {
+// 	return 3, 4
+// }
 
 // functions 
 func sum(a, b int) int{ // we can write (x,y int) only when we now that both x and y are integers. 
@@ -34,4 +54,14 @@ func sum(a, b int) int{ // we can write (x,y int) only when we now that both x a
 func sum2(a1 int, b1 int) int { // this is the defalut way. 
 	// There is something known as function signatures. If you want to return something from teh function, we need to specify the type of thing which is being returned, in our case it's int. 
 	return a1 + b1;
+}
+
+func increment(var1 int) int {
+	inc := var1 + 1;
+	return inc; 
+}
+
+// return values concept. If we have more than one return value, we wrap the type of the varialbles like this:- 
+func Getnames()(string, string){
+	return "Hello", "World";
 }
