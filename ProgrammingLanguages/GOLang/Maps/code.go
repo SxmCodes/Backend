fdd8bbe064ebdfeb612365613ff7a2a2d223e7e3
Saksham Sharma <sakshamsharma1029@gmail.com>
2025-03-20 16:00:21 +0000

    Code of Go

--- ProgrammingLanguages/GOLang/Maps/code.go
+++ ProgrammingLanguages/GOLang/Maps/code.go
@@ -11,7 +11,7 @@ ages["Mary"] = 21
 /*
 	We can perform some functions with maps and keys. 
 	1. To delete an element := delete(m, key)
-	2. To check if a key is present := value, ok := m[key]
+	2. To check if a key is present := value, ok := m[key] - here ok is boolean, if boolean is true then key is present.
 	3. To iterate over a map := for key, value := range m {}
 	4. To get the number of elements := len(m)
 	5. To create a map with initial values := map[string] int {"John": 37, "Mary": 24}
@@ -19,6 +19,27 @@ ages["Mary"] = 21
 	7. To create a map with a specific capacity := make(map[string] int, 100)
 */
 
+// name will take the username which we need to detete. Users is a map of string (usernames) and user is a structure. 
+func deleteIfNecessary(users map[string] user, name string ) (deleted bool, err error ){
+	// deleted variable will tell weather given user is deleted or not. Error aaigi toh return karega (err variable)
+	// check weather the user is present in the map or not. If not present then return false and error.
+	if _, ok := users[name]; !ok {
+		// errorf basically iik error object banata hai formatted string ke sath. Ye errors handling mai use hota hai. Hm iske sath customized erros bna sakte hai and values bhi format kar sakte hai. Also error wrapping mai bhi kaam aata hai.
+
+		return false, fmt.Errorf("User %s not found", name)
+	}
+	delete(users, name)
+	return true, nil
+}
+
+// This is the sample function which tells us how to use error handling using fmt.Errorf
+func divide(a, b int) (int, error){
+	if b == 0{
+		fmt.Errorf("Cannot divide by zero")
+		return 0, err
+	}
+	return a/b, nil
+}
 ages := map[string] int {
 	"John": 37,
 	"Mary": 24,
