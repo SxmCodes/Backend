7b00194348bfb1f02c0eeb6e8265509568741f16
Saksham Sharma <sakshamsharma1029@gmail.com>
2025-03-22 08:02:34 +0000

    Simple calculator for console

--- ProgrammingLanguages/GOLang/DoingStuff/CommandLineCalculator/code.go
+++ ProgrammingLanguages/GOLang/DoingStuff/CommandLineCalculator/code.go
@@ -1,12 +1,12 @@
-package main 
+package main
 
 import (
-	"fmt"
+	"bufio"
 	"errors"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
-	"bufio"
 )
 
 func calc(a int, b int, op string) (int, error) {
@@ -33,8 +33,32 @@ func main(){
 	fmt.Println("Enter the first number: ")
 	// we took the input as string and then converted it to integer. 
 	// input, _ := reader.ReadString('\n') is a line of code that reads a string from a bufio.Reader until a newline character (\n) is encountered. 
+	input, _ := reader.ReadString('\n') // Will read string until a new line has been encountered.
+	input = strings.TrimSpace(input) // Removes the leading and trailing white spaces from the string.
 
+	// splitting the parts into 3, because we need 2 inputs and 1 string (to tell which action to perform). 
+	parts := strings.Split(input, " ") // Splits the string into parts based on the space.
+	if len(parts) != 3 {
+		fmt.Println("Invalid input")
+		return
+	}
+
+	a, err := strconv.Atoi(parts[0]) // Converts the string to integer.
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+	
+	b, err := strconv.Atoi(parts[0]) // Converts the string to integer for parts[2]
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+
+	result, err := calc(a, b, parts[1]) // Calls the calc function with the input values.
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Printf("Result: %d\n", result)
 }
-
-// Learnings :- 
-	// 1. errors.New and Errors lib
