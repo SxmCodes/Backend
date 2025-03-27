bb9066ef65f0091aaa617285f961b2643606651f
Saksham Sharma <sakshamsharma1029@gmail.com>
2025-03-27 08:23:35 +0000

    simple console calculator

--- ProgrammingLanguages/GOLang/DoingStuff/CommandLineCalculator/code.go
+++ ProgrammingLanguages/GOLang/DoingStuff/CommandLineCalculator/code.go
@@ -33,29 +33,29 @@ func main(){
 	fmt.Println("Enter the first number: ")
 	// we took the input as string and then converted it to integer. 
 	// input, _ := reader.ReadString('\n') is a line of code that reads a string from a bufio.Reader until a newline character (\n) is encountered. 
-	input, _ := reader.ReadString('\n') // Will read string until a new line has been encountered.
-	input = strings.TrimSpace(input) // Removes the leading and trailing white spaces from the string.
-
-	// splitting the parts into 3, because we need 2 inputs and 1 string (to tell which action to perform). 
-	parts := strings.Split(input, " ") // Splits the string into parts based on the space.
-	if len(parts) != 3 {
-		fmt.Println("Invalid input")
-		return
-	}
-
-	a, err := strconv.Atoi(parts[0]) // Converts the string to integer.
-	if err != nil {
-		fmt.Println("Invalid input")
-		return
-	}
+	inputA, _ := reader.ReadString('\n') // Will read string until a new line has been encountered.
+	inputA = strings.TrimSpace(inputA) // Removes the leading and trailing white spaces from the string.
 	
-	b, err := strconv.Atoi(parts[0]) // Converts the string to integer for parts[2]
+	a, err := strconv.Atoi(inputA) // Converts the string to integer.
 	if err != nil {
 		fmt.Println("Invalid input")
 		return
 	}
+	// Taking operator as an input. 
+	fmt.Println("Enter the operator: ")
+	op, _ := reader.ReadString('\n')
+	op = strings.TrimSpace(op)
 
-	result, err := calc(a, b, parts[1]) // Calls the calc function with the input values.
+	fmt.Println("Enter the second number: ")
+	inputB, _ := reader.ReadString('\n')
+	inputB = strings.TrimSpace(inputB)
+	b, err := strconv.Atoi(inputB)
+	if err != nil {
+		fmt.Println("Invalid input")
+		return
+	}
+	// Calling the calc function.
+	result, err := calc(a, b, op) // Calls the calc function with the input values.
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
