4b5375b256acf1db3aa439fce301e3b309762534
Saksham Sharma <sakshamsharma1029@gmail.com>
2025-03-27 08:48:15 +0000

    Number guessing game - in process

---
+++ ProgrammingLanguages/GOLang/DoingStuff/NumberGuessingGame/code.go
@@ -0,0 +1,27 @@
+package main 
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main(){
+	// rand.intn returns a random int n, 0 <= n < 100 and rand.Float64 returns a random float64, 0.0 <= f < 1.0
+
+	// seed the random number generator with the current time
+	// concept of seeding and seed:- 
+	/*
+	Computers don't produce truly random numbers. Instead, they generate sequences of numbers that appear random but are actually determined by an algorithm. These are called pseudo-random numbers.
+	SEED := The "seed" is the starting value for this algorithm. Think of it as the initial input that dictates the entire sequence of "random" numbers that will follow.
+	If you use the same seed, you'll get the same sequence of numbers every time. If you use a different seed, you'll get a different sequence.
+	*/
+	
+	// hints
+
+	// limit on guessses and return too high or too low. 
+
+	// continue when user gets correct number 
+
+	// once done, the program will print the number of guesses taken.
+}
