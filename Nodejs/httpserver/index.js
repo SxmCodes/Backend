b655e00a30ff142ee0b12793924305ed8baae15f
Saksham Sharma <sakshamsharma1029@gmail.com>
2025-05-03 17:10:35 +0000

    Nodejs Series

--- Nodejs/httpserver/index.js
+++ Nodejs/httpserver/index.js
@@ -1,13 +1,26 @@
 // http server 
 const http = require("http")
 const fs = require("fs")
+const url = require("url")
+
 
 const myserver = http.createServer((req, res) => {
     // creating a log file.
     const log = `${Date.now()}: New request has been recieved!\n`
+    const myUrl = url.parse(req.url)
+    console.log(myUrl)
     fs.appendFile("log.txt", log, (err, data) => {
-        console.log(req.headers)
-        res.end("Hello from my new server!!")
+        switch (myUrl.pathname) {
+            case "/":
+                res.end("Homepage")
+                break;
+            case "/about":
+                const username = myUrl.query.myname;
+                res.end("About page")
+                break;
+            default:
+                res.end("404 not found...")
+        }
     })
 });
 
