c9dfd8838ef5a268bef42ab6b539fd03ba41c9d5
Saksham Sharma <sakshamsharma1029@gmail.com>
2025-05-13 16:23:18 +0000

    Async await

--- KiratCohort/Javascript/asynawait.js
+++ KiratCohort/Javascript/asynawait.js
@@ -1,25 +1,57 @@
 function nothing() {
-    let promise = new promise(function (resolve) {
-        setTimeout(() => {
-            console.log('Hi there');
-        }, 2000);
-    });
-    return promise;
+  let promise = new promise(function (resolve) {
+    setTimeout(() => {
+      console.log("Hi there");
+    }, 2000);
+  });
+  return promise;
 }
-  
+
 // instead of using .then() and .catch() we can use async await
 async function main() {
-    // when you do this, it's the same as saying...
-    let value = await nothing()
+  // when you do this, it's the same as saying...
+  let value = await nothing();
 
-    // This is also same...
-    nothing().then(function (value) {
-        console.log(value);
-    })
-
+  // This is also same...
+  nothing().then(function (value) {
     console.log(value);
-}
-// usually you don't write your own async function, you just call it, and wrap it around another async function. 
+  });
 
-// Today's assignment is to write own async function which does a callback to the thread of cpu. How to get into main thread and ask js to make raw async function. 
-// Next assignment is to write your own promise class, which will have resolve and duration. 
+  console.log(value);
+}
+// usually you don't write your own async function, you just call it, and wrap it around another async function.
+
+// Today's assignment is to write own async function which does a callback to the thread of cpu. How to get into main thread and ask js to make raw async function.
+// Next assignment is to write your own promise class, which will have resolve and duration.
+
+// here is one mst example...
+function myownfunction() {
+  return new Promise(function (onDone, onError) {
+    onError("Error");
+  });
+}
+
+const p = myownfunction();
+
+p.then(function () {
+  console.log("Success");
+}).catch(function () {
+  console.log("Error");
+});
+
+// one more example 
+function arthematic(a, b, fn) {
+  let value1 = a * a; 
+  let value2 = b * b;
+  fn(value1, value2);
+}
+
+arthematic(2, 3, function (value1, value2) {
+  console.log(value1 + value2);
+}
+);
+
+// returning as a promise. 
+arthematic(1, 2).then(function (value) {
+console.log(value);
+})
